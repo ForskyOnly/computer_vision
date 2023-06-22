5b169e0d67d49776bf4f453b8c2424b5f18e16dc
ForskyOnly <foulseumsky@gmail.com>
2023-06-22 22:48:10 +0200

    code commenté + docstring

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import cv2
 import numpy as np
 from PIL import Image, ImageDraw
 
-def load_model():
+def charger_model():
     """
     Charger le modèle YOLO à partir du fichier 'best.pt'.
     
@@ -26,35 +26,35 @@ class VideoTransformer(VideoTransformerBase):
         confidence_threshold (float): Seuil de confiance pour la prédiction.
         overlap_threshold (float): Seuil de recouvrement pour la suppression non-maximale.
         """
-        self.model = load_model()
+        self.model = charger_model()
         self.confidence_threshold = confidence_threshold
         self.overlap_threshold = overlap_threshold
 
-    def transform(self, frame):
+    def transform(self, cadre):
         """
-        Transformer un frame de la vidéo en détectant les objets à l'aide du modèle YOLO et en dessinant les résultats.
+        Transformer un cadre de la vidéo en détectant les objets à l'aide du modèle YOLO et en dessinant les résultats.
 
         Args:
-        frame (np.ndarray): Frame de la vidéo à analyser.
+        cadre (np.ndarray): cadre de la vidéo à analyser.
 
         Returns:
         img (np.ndarray): Image avec les prédictions dessinées.
         """
-        img = frame.to_ndarray(format="bgr24")
+        img = cadre.to_ndarray(format="bgr24")
 
-        # Appliquer le modèle
+        # Applique le modèle
         preds = self.model.predict(source=img, conf=self.confidence_threshold, iou=self.overlap_threshold, save=False)
 
-        # Dessiner les résultats sur l'image
-        for i, result in enumerate(preds):
-            for j, box in enumerate(result.boxes.data):
+        # Dessine les résultats sur l'image
+        for i, resultat in enumerate(preds):
+            for j, box in enumerate(resultat.boxes.data):
                 cv2.rectangle(img,
                               (int(box[0]), int(box[1])),
                               (int(box[0] + box[2]), int(box[1] + box[3])),
                               (255, 0, 0),
                               2)
                 cv2.putText(img, 
-                            f"Classe : {result.names[int(box[5])]}, Confiance : {box[4]}", 
+                            f"Classe : {resultat.names[int(box[5])]}, Confiance : {box[4]}", 
                             (int(box[0]), int(box[1]-10)), 
                             cv2.FONT_HERSHEY_SIMPLEX, 
                             0.9, 
@@ -63,7 +63,7 @@ class VideoTransformer(VideoTransformerBase):
 
         return img
 
-def draw_preds(image, preds):
+def dessiner_predictions(image, preds):
     """
     Dessiner les prédictions sur l'image.
 
@@ -74,20 +74,20 @@ def draw_preds(image, preds):
     Returns:
     image (PIL.Image): Image avec les prédictions dessinées.
     """
-    draw = ImageDraw.Draw(image)
+    dessine = ImageDraw.Draw(image)
 
-    for i, result in enumerate(preds):
-        for j, box in enumerate(result.boxes.data):
-            # Convertion des coordonnées des boites
+    for i, resultat in enumerate(preds):
+        for j, box in enumerate(resultat.boxes.data):
+            # Convertie les coordonnées des boites
             x1, y1 = int(box[0]), int(box[1])
             x2, y2 = int(box[0] + box[2]), int(box[1] + box[3])
             
             # Dessine le rectangle
-            draw.rectangle([x1, y1, x2, y2], outline=(255, 0, 0), width=2)
-            text = f"Classe : {result.names[int(box[5])]}, Confiance : {box[4]}"
+            dessine.rectangle([x1, y1, x2, y2], outline=(255, 0, 0), width=2)
+            text = f"Classe : {resultat.names[int(box[5])]}, Confiance : {box[4]}"
             
-            # Ajout du texte
-            draw.text((x1, y1-10), text, fill=(36, 255, 12))
+            # Ajoute le texte
+            dessine.text((x1, y1-10), text, fill=(36, 255, 12))
 
     return image
 
@@ -98,46 +98,46 @@ def app():
     st.title("Alphabet du langage des signes")
     st.sidebar.title("Réglages")
 
-    model = load_model()
+    model = charger_model()
 
-    # Paramètres conf et iou
+    # Paramètres CONF et IOU
     confidence_threshold = st.sidebar.slider("Confidence Threshold:", 0.0, 1.0, 0.3, 0.01)
     overlap_threshold = st.sidebar.slider("Overlap Threshold: ", 0.0, 1.0, 0.2, 0.01)
 
     option = st.sidebar.selectbox("Méthode de détéction", ("Image", "Camera"))
 
     if option == "Image":
-        uploaded_file = st.sidebar.file_uploader("choisir une image", type=['png', 'jpg', 'jpeg'])
-        if uploaded_file is not None:
-            image = Image.open(uploaded_file)
+        charge_img = st.sidebar.file_uploader("choisir une image", type=['png', 'jpg', 'jpeg'])
+        if charge_img is not None:
+            image = Image.open(charge_img)
 
-            rotate_direction = st.sidebar.radio("Rotation", ["Original", "90 degrés", "-90 degrés"])
-            if rotate_direction == "90 degrés":
+            direction_rota = st.sidebar.radio("Rotation", ["Original", "90 degrés", "-90 degrés"])
+            if direction_rota == "90 degrés":
                 image = image.rotate(-90)
-            elif rotate_direction == "-90 degrés":
+            elif direction_rota == "-90 degrés":
                 image = image.rotate(90)
 
             if st.button("Prédire"):
-                frame = np.array(image)
+                cadre = np.array(image)
 
                 # Effectue les prédictions sur l'image pivotée
-                preds = model.predict(source=frame, conf=confidence_threshold, iou=overlap_threshold, save=False)
+                preds = model.predict(source=cadre, conf=confidence_threshold, iou=overlap_threshold, save=False)
 
                 # Dessine les prédictions sur l'image
-                image_with_preds = draw_preds(image, preds)
+                image_avec_pred = dessiner_predictions(image, preds)
 
                 # Affiche l'image avec les prédictions
-                st.image(image_with_preds, caption="Image prédite", use_column_width=True)
+                st.image(image_avec_pred, caption="Image prédite", use_column_width=True)
 
                 # Afficher les prédictions
-                for i, result in enumerate(preds):
+                for i, resultat in enumerate(preds):
                     st.write("Résultat:")
-                    for j, box in enumerate(result.boxes.data):
+                    for j, box in enumerate(resultat.boxes.data):
                         st.write(f"Position (x, y) : ({box[0]}, {box[1]})")
                         st.write(f"Largeur : {box[2]}")
                         st.write(f"Hauteur : {box[3]}")
                         st.write(f"Confiance : {box[4]}")
-                        st.write(f"Classe : {result.names[int(box[5])]}")
+                        st.write(f"Classe : {resultat.names[int(box[5])]}")
 
     elif option == "Camera":
         webrtc_streamer(key="example", video_transformer_factory=lambda: VideoTransformer(confidence_threshold, overlap_threshold))
